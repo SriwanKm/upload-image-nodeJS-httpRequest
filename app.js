5df6eac7a97041fd7cc933075430e396cf3c86b9
SriwanKm <kcqkoo@gmail.com>
2021-05-12 20:55:18 -0700

    complete project

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+let http = require('http');
+let formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, '/uploadedImg');
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-    res.sendFile('/index.html',{ root: __dirname })
-})
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+if (!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath);
+}
+
+http.createServer(function (req, res) {
+    if (req.url === '/fileupload') {
+        const form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            const oldpath = files.filetoupload.path;
+            const newpath = dirPath + "/" + files.filetoupload.name;
+            // The uploaded image file will be added to the folder name "uploadedImg" in this project
+
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(`<head><title>Upload Image</title></head><body style="background-image: url('https://images.pexels.com/photos/3691528/pexels-photo-3691528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'); background-size: cover">`);
+        res.write('<div style="text-align: left; margin: 300px"><h1 style="color: royalblue">Upload Image</h1>');
+
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit" style="margin-top: 10px; background-color: royalblue; color: white">');
+        res.write('</form></div></body>');
+        return res.end();
+    }
+}).listen(8080);
+
+
